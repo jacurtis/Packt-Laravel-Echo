86320677bd1c4eac4a855c6c83f631e54746d15f
J. Alexander Curtis <jalexandercurtis@gmail.com>
2017-12-08 21:06:35 -0700

    4.5 Part1 - Added comment retrieval and database change to add api_token

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -20,6 +20,7 @@ $factory->define(App\User::class, function (Faker $faker) {
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'password' => $password ?: $password = bcrypt('secret'),
+        'api_token' => str_random(60),
         'remember_token' => str_random(10),
     ];
 });
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -22,6 +22,25 @@
         </li>
       </ul>
     </div>
+
+    <h3>Comments:</h3>
+    <form action="{{ 'api/posts/'.$post->id.'/comment' }}" method="POST" style="margin-bottom:20px;">
+      <textarea id="commentBody" class="form-control" rows="3" name="body" placeholder="Contribute your two cents."></textarea>
+    </form>
+    <div class="media" style="margin-top:20px;">
+      <div class="media-left">
+        <a href="#">
+          <img class="media-object" src="http://placeimg.com/80/80" alt="...">
+        </a>
+      </div>
+      <div class="media-body">
+        <h4 class="media-heading">Joey said...</h4>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <span style="color: #aaa;">on March 5th, 2014</span>
+      </div>
+    </div>
   </div>
 @endsection
 
@@ -31,12 +50,29 @@
       el: '#app',
       data: {
         viewers: [],
-        count: 0
+        count: 0,
+        post: {!! json_encode($post) !!},
+        user: {!! json_encode(Auth::user()) !!},
+        comments: {}
       },
       mounted() {
         this.listen();
+        this.getComments();
       },
       methods: {
+        getComments() {
+          axios.get('/api/posts/'+this.post.id+'/comments', {
+            params: {
+              api_token: this.user.api_token
+            }
+          })
+          .then(function (response) {
+            this.comments = response.data;
+          })
+          .catch(function (error) {
+            console.log(error);
+          })
+        },
         listen() {
           Echo.join('posts.'+'{{ $post->id }}')
               .here((users) => {
